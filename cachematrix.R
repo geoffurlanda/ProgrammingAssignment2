12a3c00f329b6bef0e27d4eb5bfef3b401ba170e
GRU <geoffreyurlanda@Geoffreys-MacBook-Air.local>
2015-05-22 22:36:08 +0800

    update 1
    added code for makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,34 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## makeCacheMatrix - function that creates a cache of matrix input
 makeCacheMatrix <- function(x = matrix()) {
+
+  ## function gets an input matrix x
+  
+  ## set initial value of matrixInverseCache to NULL
+  matrixInverseCache <- NULL
+  
+  ## this function sets the matrix
+  set <- function(newMatrix) {
+    x <<- newMatrix
+    matrixInverseCache <<- NULL
+  }
+  
+  ## this function gets the matrix
+  get <- function() {
+    x
+  }
+  
+  ## this function caches the inverse of the matrix
+  cacheMatrixInverse <- function(solve) {
+    matrixInverseCache <<- solve
+  }
+  
+  ## this function gets the cached inverse of the matrix
+  getCacheMatrixInverse <- function() {
+    matrixInverseCache
+  }
 
 }
 
